b175dceee0df986384f2f884b43837d651718616
SivaPriya8765 <sivakrishna.penumarthy@gmail.com>
2026-01-03 21:06:29 +0530

    push

--- new.py
+++ new.py
@@ -1,7 +1,8 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 # Read from environment
 api_key = os.environ.get('API_KEY')
 database = os.environ.get('DATABASE_NAME', 'default.db')
 
-print(f"Using database: {database}")
+print(f"Using database: {api_key}")
